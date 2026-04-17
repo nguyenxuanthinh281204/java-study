93b73a87e44d37ecfb0eb3bdffcb87043bb8253b
thinhnguyenxuan <ngxthinh271@gmail.com>
2026-04-18 00:18:25 +0700

    study constructor, artribute,..

--- src/chapter4/Student.java
+++ src/chapter4/Student.java
@@ -4,6 +4,15 @@ public class Student {
 	String name;
 	int age;
 
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(String name1, int age1) {
+		this.name = name;
+		this.age = age;
+	}
+
 	public void learnJava() {
 		System.out.println("Learn java");
 
