5510f655dcdbe1ea58bf344c3c73289014498cf5
thinhnguyenxuan <ngxthinh271@gmail.com>
2026-04-28 21:50:25 +0700

    Study ArrayList and Generics

---
+++ src/chapter5/Video33.java
@@ -0,0 +1,13 @@
+package chapter5;
+
+import java.util.ArrayList;
+
+public class Video33 {
+	public static void main(String[] args) {
+		ArrayList a = new ArrayList();
+		a.add(2);
+		a.add("em");
+
+		System.out.println(a.toString());
+	}
+}
